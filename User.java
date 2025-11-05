f14d7d390a085aa64f7ec9ac14e53dc713bcbc70
ojasbarhate24 <ojas.barhate24@pccoepune.org>
2025-11-05 09:32:59 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String email;
+    private String password;
+    private String role;
+
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getRole() { return role; }
+}
